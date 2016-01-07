bf24cdb687f558b5d551140babd871ca75127dae
mateusz.kaczmarek <mkaczmarek.pl@gmail.com>
2016-01-07 09:55:57 +0100

    PermMissingElem with streams

--- PrivateGithub/src/pl/kaczor/codility/lesssons/timecplx/PermMissingElem.java
+++ PrivateGithub/src/pl/kaczor/codility/lesssons/timecplx/PermMissingElem.java
@@ -1,12 +1,15 @@
 package pl.kaczor.codility.lesssons.timecplx;
 
+import java.util.Arrays;
+
 public class PermMissingElem {
     public int solution(int[] A) {
         long sum = 0;
         long properSum;
-        for (int a : A) {
-            sum += a;
-        }
+//        for (int a : A) {
+//            sum += a;
+//        }
+        sum = Arrays.stream(A).sum();
 
         properSum = (long) (A.length + 1) * (A.length + 2) / 2;
 
